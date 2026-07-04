3882619f70968192bfdb25256a5b46cbefdaeb0f
BabuLahade <babulahade@gmail.com>
2026-07-04 18:07:50 +0530

    varaible file updated for service accoint IAM roles

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -56,3 +56,33 @@ variable "services" {
   type        = list(string)
   default     = ["market-data-ingestion", "portfolio-calculator", "alert-api"]
 }
+
+// phase 4 variales variable "oidc_provider_arn" {
+  description = "OIDC provider ARN from EKS module output"
+  type        = string
+}
+
+variable "oidc_provider_url" {
+  description = "OIDC provider URL from EKS module output — without https://"
+  type        = string
+}
+
+variable "sqs_queue_arn" {
+  description = "ARN of the SQS market data queue"
+  type        = string
+}
+
+variable "market_data_table_arn" {
+  description = "ARN of fintech-market-data DynamoDB table"
+  type        = string
+}
+
+variable "portfolios_table_arn" {
+  description = "ARN of fintech-portfolios DynamoDB table"
+  type        = string
+}
+
+variable "alerts_table_arn" {
+  description = "ARN of fintech-alerts DynamoDB table"
+  type        = string
+}
